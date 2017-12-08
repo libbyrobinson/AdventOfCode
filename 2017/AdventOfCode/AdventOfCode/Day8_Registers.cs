651d48968e4a893bb04e808a11d507a3e91bec85
Libby Robinson <libbyr@iqmetrix.com>
2017-12-08 15:01:19 -0800

    Day8

---
+++ 2017/AdventOfCode/AdventOfCode/Day8_Registers.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    public class Day8_Registers
+    {
+        public int GetLargestRegistryValueAfterInstructions(IEnumerable<RegisterInstruction> instructions)
+        {
+            var registers = new Dictionary<string, int>();
+            foreach (var instruction in instructions)
+            {
+                RunInstructionOnRegister(registers, instruction);
+            }
+
+            return registers.Values.Max();
+        }
+
+        public int GetLargestRegistryValueDuringInstructions(IEnumerable<RegisterInstruction> instructions)
+        {
+            var registers = new Dictionary<string, int>();
+            var largest = 0;
+            foreach (var instruction in instructions)
+            {
+                RunInstructionOnRegister(registers, instruction);
+                var currentMax = registers.Values.Max();
+                if (currentMax > largest)
+                {
+                    largest = currentMax;
+                }
+            }
+
+            return largest;
+        }
+
+        private static void RunInstructionOnRegister(IDictionary<string, int> registers, RegisterInstruction instruction)
+        {
+            if (!registers.ContainsKey(instruction.RegisterToModify))
+            {
+                registers.Add(instruction.RegisterToModify, 0);
+            }
+            if (!registers.ContainsKey(instruction.ConditionRegister))
+            {
+                registers.Add(instruction.ConditionRegister, 0);
+            }
+
+            bool conditionMet;
+            switch (instruction.ConditionOperand)
+            {
+                case OperandType.GreaterThan:
+                    conditionMet = registers[instruction.ConditionRegister] > instruction.ConditionAmount;
+                    break;
+                case OperandType.LessThan:
+                    conditionMet = registers[instruction.ConditionRegister] < instruction.ConditionAmount;
+                    break;
+                case OperandType.GreaterThanOrEqualTo:
+                    conditionMet = registers[instruction.ConditionRegister] >= instruction.ConditionAmount;
+                    break;
+                case OperandType.LessThanOrEqualTo:
+                    conditionMet = registers[instruction.ConditionRegister] <= instruction.ConditionAmount;
+                    break;
+                case OperandType.EqualTo:
+                    conditionMet = registers[instruction.ConditionRegister] == instruction.ConditionAmount;
+                    break;
+                case OperandType.NotEqualTo:
+                    conditionMet = registers[instruction.ConditionRegister] != instruction.ConditionAmount;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            if (conditionMet)
+            {
+                switch (instruction.Operation)
+                {
+                    case OperationType.Increase:
+                        registers[instruction.RegisterToModify] += instruction.Amount;
+                        break;
+                    case OperationType.Decrease:
+                        registers[instruction.RegisterToModify] -= instruction.Amount;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+    }
+
+    public class RegisterInstruction
+    {
+        public RegisterInstruction(string rawInstruction)
+        {
+            var instructions = rawInstruction.Split(' ');
+
+            RegisterToModify = instructions[0];
+
+            switch (instructions[1])
+            {
+                case "inc":
+                    Operation = OperationType.Increase;
+                    break;
+                case "dec":
+                    Operation = OperationType.Decrease;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            Amount = int.Parse(instructions[2]);
+            ConditionRegister = instructions[4];
+
+            switch (instructions[5])
+            {
+                case "<":
+                    ConditionOperand = OperandType.LessThan;
+                    break;
+                case "<=":
+                    ConditionOperand = OperandType.LessThanOrEqualTo;
+                    break;
+                case ">":
+                    ConditionOperand = OperandType.GreaterThan;
+                    break;
+                case ">=":
+                    ConditionOperand = OperandType.GreaterThanOrEqualTo;
+                    break;
+                case "==":
+                    ConditionOperand = OperandType.EqualTo;
+                    break;
+                case "!=":
+                    ConditionOperand = OperandType.NotEqualTo;
+                    break;
+            }
+
+            ConditionAmount = int.Parse(instructions[6]);
+        }
+
+        public string RegisterToModify { get; set; }
+        public OperationType Operation { get; set; }
+        public int Amount { get; set; }
+        public string ConditionRegister { get; set; }
+        public OperandType ConditionOperand { get; set; }
+        public int ConditionAmount { get; set; }
+    }
+
+    public enum OperandType
+    {
+        GreaterThan,
+        LessThan,
+        GreaterThanOrEqualTo,
+        LessThanOrEqualTo,
+        EqualTo,
+        NotEqualTo
+    }
+
+    public enum OperationType
+    {
+        Increase, 
+        Decrease
+    }
+}
---
+++ 2017/AdventOfCode/AdventOfCode/Tests/Day8_Tests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.PuzzleInput;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode
+{
+    public class Day8_Tests
+    {
+        private Day8_Registers _sut;
+
+        public Day8_Tests()
+        {
+            _sut = new Day8_Registers();
+        }
+
+        [Theory]
+        [InlineData("b inc 5 if a > 1", "b", OperationType.Increase, 5, "a", OperandType.GreaterThan, 1)]
+        [InlineData("a inc 1 if b < 5", "a", OperationType.Increase, 1, "b", OperandType.LessThan, 5)]
+        [InlineData("c dec -10 if a >= 1", "c", OperationType.Decrease, -10, "a", OperandType.GreaterThanOrEqualTo, 1)]
+        [InlineData("c inc -20 if c == 10", "c", OperationType.Increase, -20, "c", OperandType.EqualTo, 10)]
+        public void ParseInstruction(
+            string rawInstruction,
+            string registerToModify,
+            OperationType operation,
+            int amount,
+            string conditionRegister,
+            OperandType conditionOperand,
+            int conditionAmount)
+        {
+            var instruction = new RegisterInstruction(rawInstruction);
+
+            instruction.RegisterToModify.Should().Be(registerToModify);
+            instruction.Operation.Should().Be(operation);
+            instruction.Amount.Should().Be(amount);
+            instruction.ConditionRegister.Should().Be(conditionRegister);
+            instruction.ConditionOperand.Should().Be(conditionOperand);
+            instruction.ConditionAmount.Should().Be(conditionAmount);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetDay8Data))]
+        public void CheckLargestValues(IEnumerable<RegisterInstruction> instructions, int expectedAfter, int expectedDuring)
+        {
+            _sut.GetLargestRegistryValueAfterInstructions(instructions).Should().Be(expectedAfter);
+            _sut.GetLargestRegistryValueDuringInstructions(instructions).Should().Be(expectedDuring);
+        }
+        
+        public static IEnumerable<object[]> GetDay8Data()
+        {
+            yield return new object[]
+            {
+                new List<RegisterInstruction>
+                {
+                    new RegisterInstruction("b inc 5 if a > 1"),
+                    new RegisterInstruction("a inc 1 if b < 5"),
+                    new RegisterInstruction("c dec -10 if a >= 1"),
+                    new RegisterInstruction("c inc -20 if c == 10")
+                },
+                1,
+                10
+            };
+
+            var input = PuzzleInputParser.Parse("Day8Input.txt");
+            var lines = input
+                .Split(new[] { "\r\n" }, StringSplitOptions.None)
+                .ToList();
+
+            yield return new object[]
+            {
+                lines.Select(l => new RegisterInstruction(l)),
+                3745,
+                4644
+            };
+        }
+    }
+}
