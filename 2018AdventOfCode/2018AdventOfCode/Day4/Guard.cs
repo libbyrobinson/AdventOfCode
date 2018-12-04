ba2234378db4a6f96f4668d649699e5e4ecec177
Libby Robinson <libbyr@iqmetrix.com>
2018-12-04 10:09:24 -0800

    Cleanup Day4

---
+++ 2018AdventOfCode/2018AdventOfCode/Day4/Guard.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MoreLinq.Extensions;
+
+namespace _2018AdventOfCode.Day4
+{
+    public class Guard
+    {
+        public Guard(int id)
+        {
+            Id = id;
+            SleepingMinuteTracker = new Dictionary<int, int>();
+            for (var minute = 0; minute < 59; minute++)
+            {
+                SleepingMinuteTracker.Add(minute, 0);
+            }
+        }
+
+        public int Id { get; set; }
+        public Dictionary<int, int> SleepingMinuteTracker { get; set; }
+
+        public int TotalMinutesAsleep
+        {
+            get { return SleepingMinuteTracker.Sum(m => m.Value); }
+        }
+
+        public int MinuteAsleepMost
+        {
+            get { return SleepingMinuteTracker.MaxBy(s => s.Value).First().Key; }
+        }
+
+        public int MostTimeAsleepAtAnyMinute
+        {
+            get { return SleepingMinuteTracker.MaxBy(s => s.Value).First().Value; }
+        }
+
+        public void AddSleepingMinutes(int fallAsleepMinute, int wakeMinute)
+        {
+            for (var minute = fallAsleepMinute; minute < wakeMinute; minute++)
+            {
+                SleepingMinuteTracker[minute]++;
+            }
+        }
+    }
+}
--- 2018AdventOfCode/2018AdventOfCode/Day4/GuardShiftTracker.cs
+++ 2018AdventOfCode/2018AdventOfCode/Day4/GuardShiftTracker.cs
@@ -50,46 +50,7 @@ namespace _2018AdventOfCode.Day4
 
         public Guard FindGuardMostFrequentlyAsleepAtSameMinute()
         {
-            return _guards.MaxBy(g => g.Value.MostDaysAsleepAtSameMinute).First().Value;
-        }
-    }
-
-    public class Guard
-    {
-        public Guard(int id)
-        {
-            Id = id;
-            SleepingMinuteTracker = new Dictionary<int, int>();
-            for (var minute = 0; minute < 59; minute++)
-            {
-                SleepingMinuteTracker.Add(minute, 0);
-            }
-        }
-
-        public int Id { get; set; }
-        public Dictionary<int, int> SleepingMinuteTracker { get; set; }
-
-        public int TotalMinutesAsleep
-        {
-            get { return SleepingMinuteTracker.Sum(m => m.Value); }
-        }
-
-        public int MinuteAsleepMost
-        {
-            get { return SleepingMinuteTracker.MaxBy(s => s.Value).First().Key; }
-        }
-
-        public int MostDaysAsleepAtSameMinute
-        {
-            get { return SleepingMinuteTracker.MaxBy(s => s.Value).First().Value; }
-        }
-
-        public void AddSleepingMinutes(int fallAsleepMinute, int wakeMinute)
-        {
-            for (var minute = fallAsleepMinute; minute < wakeMinute; minute++)
-            {
-                SleepingMinuteTracker[minute]++;
-            }
+            return _guards.MaxBy(g => g.Value.MostTimeAsleepAtAnyMinute).First().Value;
         }
     }
 }
