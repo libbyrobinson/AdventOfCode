18f8a73e32f33aa7babf0af5081260062c8a8bc9
Libby Robinson <libbyr@iqmetrix.com>
2018-12-04 10:05:34 -0800

    Day4

---
+++ 2018AdventOfCode/2018AdventOfCode/Day4/GuardShiftTracker.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using MoreLinq.Extensions;
+
+namespace _2018AdventOfCode.Day4
+{
+    public class GuardShiftTracker
+    {
+        private readonly Dictionary<int, Guard> _guards;
+
+        public GuardShiftTracker(IEnumerable<string> rawGuardShiftLogs)
+        {
+            _guards = new Dictionary<int, Guard>();
+
+            var orderedLogs = rawGuardShiftLogs
+                .Select(l => l.Split(']'))
+                .ToDictionary(dateAndAction => DateTime.Parse(dateAndAction[0].TrimStart('[')), dateAndAction => dateAndAction[1])
+                .OrderBy(d => d.Key);
+
+            var currentGuardId = 0;
+            var fallAsleepMinute = 0;
+            foreach (var orderedLog in orderedLogs)
+            {
+                if (orderedLog.Value.Contains("Guard #"))
+                {
+                    currentGuardId = int.Parse(orderedLog.Value.Split('#')[1].Split(' ')[0]);
+                }
+                else if (orderedLog.Value.Contains("falls asleep"))
+                {
+                    fallAsleepMinute = orderedLog.Key.Minute;
+                }
+                else if (orderedLog.Value.Contains("wakes up"))
+                {
+                    if (!_guards.ContainsKey(currentGuardId))
+                    {
+                        _guards.Add(currentGuardId, new Guard(currentGuardId));
+                    }
+
+                    _guards[currentGuardId].AddSleepingMinutes(fallAsleepMinute, orderedLog.Key.Minute);
+                }
+            }
+        }
+
+        public Guard FindGuardWithMostMinutesAsleep()
+        {
+            return _guards.MaxBy(g => g.Value.TotalMinutesAsleep).First().Value;
+        }
+
+        public Guard FindGuardMostFrequentlyAsleepAtSameMinute()
+        {
+            return _guards.MaxBy(g => g.Value.MostDaysAsleepAtSameMinute).First().Value;
+        }
+    }
+
+    public class Guard
+    {
+        public Guard(int id)
+        {
+            Id = id;
+            SleepingMinuteTracker = new Dictionary<int, int>();
+            for (var minute = 0; minute < 59; minute++)
+            {
+                SleepingMinuteTracker.Add(minute, 0);
+            }
+        }
+
+        public int Id { get; set; }
+        public Dictionary<int, int> SleepingMinuteTracker { get; set; }
+
+        public int TotalMinutesAsleep
+        {
+            get { return SleepingMinuteTracker.Sum(m => m.Value); }
+        }
+
+        public int MinuteAsleepMost
+        {
+            get { return SleepingMinuteTracker.MaxBy(s => s.Value).First().Key; }
+        }
+
+        public int MostDaysAsleepAtSameMinute
+        {
+            get { return SleepingMinuteTracker.MaxBy(s => s.Value).First().Value; }
+        }
+
+        public void AddSleepingMinutes(int fallAsleepMinute, int wakeMinute)
+        {
+            for (var minute = fallAsleepMinute; minute < wakeMinute; minute++)
+            {
+                SleepingMinuteTracker[minute]++;
+            }
+        }
+    }
+}
---
+++ 2018AdventOfCode/2018AdventOfCode/Day4/GuardShiftTrackerTests.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace _2018AdventOfCode.Day4
+{
+    public class GuardShiftTrackerTests
+    {
+        [Fact]
+        public void ShouldFindSleepiestGuardFromExample()
+        {
+            var exampleShiftLogs = new List<string>
+            {
+                "[1518 - 11 - 01 00:00] Guard #10 begins shift",
+                "[1518 - 11 - 01 00:05] falls asleep",
+                "[1518 - 11 - 01 00:25] wakes up",
+                "[1518 - 11 - 01 00:30] falls asleep",
+                "[1518 - 11 - 01 00:55] wakes up",
+                "[1518 - 11 - 01 23:58] Guard #99 begins shift",
+                "[1518 - 11 - 02 00:40] falls asleep",
+                "[1518 - 11 - 02 00:50] wakes up",
+                "[1518 - 11 - 03 00:05] Guard #10 begins shift",
+                "[1518 - 11 - 03 00:24] falls asleep",
+                "[1518 - 11 - 03 00:29] wakes up",
+                "[1518 - 11 - 04 00:02] Guard #99 begins shift",
+                "[1518 - 11 - 04 00:36] falls asleep",
+                "[1518 - 11 - 04 00:46] wakes up",
+                "[1518 - 11 - 05 00:03] Guard #99 begins shift",
+                "[1518 - 11 - 05 00:45] falls asleep",
+                "[1518 - 11 - 05 00:55] wakes up"
+            };
+
+            var sut = new GuardShiftTracker(exampleShiftLogs);
+
+            var guard = sut.FindGuardWithMostMinutesAsleep();
+
+            guard.Id.Should().Be(10);
+            guard.MinuteAsleepMost.Should().Be(24);
+            (guard.Id * guard.MinuteAsleepMost).Should().Be(240);
+        }
+
+        [Fact]
+        public void ShouldFindSleepiestGuardFromBigInput()
+        {
+            var shiftLogs = PuzzleInputParser.ParseStrings("Day4/Input.txt");
+            var sut = new GuardShiftTracker(shiftLogs);
+
+            var guard = sut.FindGuardWithMostMinutesAsleep();
+
+            guard.Id.Should().Be(3557);
+            guard.MinuteAsleepMost.Should().Be(30);
+            (guard.Id * guard.MinuteAsleepMost).Should().Be(106710);
+        }
+
+        [Fact]
+        public void ShouldFindGuardMostFrequentlyAsleepAtSameMinuteFromExample()
+        {
+            var exampleShiftLogs = new List<string>
+            {
+                "[1518 - 11 - 01 00:00] Guard #10 begins shift",
+                "[1518 - 11 - 01 00:05] falls asleep",
+                "[1518 - 11 - 01 00:25] wakes up",
+                "[1518 - 11 - 01 00:30] falls asleep",
+                "[1518 - 11 - 01 00:55] wakes up",
+                "[1518 - 11 - 01 23:58] Guard #99 begins shift",
+                "[1518 - 11 - 02 00:40] falls asleep",
+                "[1518 - 11 - 02 00:50] wakes up",
+                "[1518 - 11 - 03 00:05] Guard #10 begins shift",
+                "[1518 - 11 - 03 00:24] falls asleep",
+                "[1518 - 11 - 03 00:29] wakes up",
+                "[1518 - 11 - 04 00:02] Guard #99 begins shift",
+                "[1518 - 11 - 04 00:36] falls asleep",
+                "[1518 - 11 - 04 00:46] wakes up",
+                "[1518 - 11 - 05 00:03] Guard #99 begins shift",
+                "[1518 - 11 - 05 00:45] falls asleep",
+                "[1518 - 11 - 05 00:55] wakes up"
+            };
+
+            var sut = new GuardShiftTracker(exampleShiftLogs);
+
+            var guard = sut.FindGuardMostFrequentlyAsleepAtSameMinute();
+
+            guard.Id.Should().Be(99);
+            guard.MinuteAsleepMost.Should().Be(45);
+            (guard.Id * guard.MinuteAsleepMost).Should().Be(4455);
+        }
+
+        [Fact]
+        public void ShouldFindGuardMostFrequentlyAsleepAtSameMinuteFromBigInput()
+        {
+            var shiftLogs = PuzzleInputParser.ParseStrings("Day4/Input.txt");
+            var sut = new GuardShiftTracker(shiftLogs);
+
+            var guard = sut.FindGuardMostFrequentlyAsleepAtSameMinute();
+
+            guard.Id.Should().Be(269);
+            guard.MinuteAsleepMost.Should().Be(39);
+            (guard.Id * guard.MinuteAsleepMost).Should().Be(10491);
+        }
+    }
+}
