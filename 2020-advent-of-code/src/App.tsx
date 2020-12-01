bdf1f5f191c29f7686e8a23d58a74badb6b57d07
Libby Robinson <libbyrobinson@asana.com>
2020-11-30 22:26:35 -0800

    2020 day 1

---
+++ 2020-advent-of-code/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Day1 from './days/day1';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Advent of Code 2020</h1>
+      </header>
+      <body>
+        <div>
+          <Day1/>
+        </div>
+      </body>
+    </div>
+  );
+}
+
+export default App;
