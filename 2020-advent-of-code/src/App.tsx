9eb084c84a998f6e3e0df6bffa0c173587c95f84
Libby Robinson <libbygrobinson@gmail.com>
2020-12-06 10:05:05 -0800

    Day6

--- 2020-advent-of-code/src/App.tsx
+++ 2020-advent-of-code/src/App.tsx
@@ -5,6 +5,7 @@ import Day2 from "./days/day2";
 import Day3 from "./days/day3";
 import Day4 from "./days/day4";
 import Day5 from "./days/day5";
+import Day6 from "./days/day6";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Day3 />
         <Day4 />
         <Day5 />
+        <Day6 />
       </div>
     </div>
   );
