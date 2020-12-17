fa7a9a1303f9c58c2d87eab27ee3c5213542520a
Libby Robinson <libbygrobinson@gmail.com>
2020-12-17 12:48:09 -0800

    Day 17 part 2

--- 2020-advent-of-code/src/days/day17.tsx
+++ 2020-advent-of-code/src/days/day17.tsx
@@ -8,75 +8,67 @@ const input = [
   "#.####..",
   "#....##.",
 ];
-const example = [".#.", "..#", "###"];
 
-type Coordinate = { x: number; y: number; z: number };
 type EnergySource = { [key: string]: boolean };
 
 function Day17() {
   const generateNeighborOffsets = (dimensions: number) => {
-    let offsets: Coordinate[] = [];
-    for (let i = 0; i < Math.pow(dimensions, dimensions); i++) {
-      const radixValues = i
-        .toString(dimensions)
+    let offsets: number[][] = [];
+    for (let i = 0; i < Math.pow(3, dimensions); i++) {
+      const offset = i
+        .toString(3)
         .padStart(dimensions, "0")
         .split("")
-        .map((x) => parseInt(x));
-      if (radixValues.every((v) => v === 1)) {
+        .map((x) => parseInt(x))
+        .map((x) => x - 1);
+
+      if (offset.every((v) => v === 0)) {
         continue;
       }
-      offsets.push({
-        x: radixValues[0] - 1,
-        y: radixValues[1] - 1,
-        z: radixValues[2] - 1,
-      });
+
+      offsets.push(offset);
     }
     return offsets;
   };
 
-  const neighborOffsets = generateNeighborOffsets(3);
+  const toKey = (coordinates: number[]) => coordinates.join(":");
 
-  const toKey = (x: number, y: number, z: number) => `${x}:${y}:${z}`;
-
-  const toCoordinate = (key: string): Coordinate => {
-    const coordinate = key.split(":");
-    return {
-      x: Number(coordinate[0]),
-      y: Number(coordinate[1]),
-      z: Number(coordinate[2]),
-    };
+  const toCoordinate = (key: string): number[] => {
+    return key.split(":").map((x) => parseInt(x));
   };
 
   const countActiveNeighbors = (
-    coordinate: Coordinate,
-    energySource: EnergySource
+    coordinate: number[],
+    energySource: EnergySource,
+    neighborOffsets: number[][]
   ) => {
     let activeNeighbors = 0;
-    neighborOffsets.forEach((offset: Coordinate) => {
-      if (
-        energySource[
-          toKey(
-            coordinate.x + offset.x,
-            coordinate.y + offset.y,
-            coordinate.z + offset.z
-          )
-        ]
-      ) {
+    neighborOffsets.forEach((offset: number[]) => {
+      const offsetCoordinate = coordinate.map(
+        (dimension: number, index: number) => {
+          return dimension + offset[index];
+        }
+      );
+      if (energySource[toKey(offsetCoordinate)]) {
         activeNeighbors++;
       }
     });
     return activeNeighbors;
   };
 
-  const markInactiveNeighbors = (energySource: EnergySource) => {
+  const markInactiveBoundary = (
+    energySource: EnergySource,
+    neighborOffsets: number[][]
+  ) => {
     Object.entries(energySource).forEach((value: [string, boolean]) => {
-      neighborOffsets.forEach((offset: Coordinate) => {
+      neighborOffsets.forEach((offset: number[]) => {
         const coordinate = toCoordinate(value[0]);
-        const boundaryKey = toKey(
-          coordinate.x + offset.x,
-          coordinate.y + offset.y,
-          coordinate.z + offset.z
+        const offsetCoordinate = coordinate.map(
+          (dimension: number, index: number) => {
+            return dimension + offset[index];
+          }
         );
+        const boundaryKey = toKey(offsetCoordinate);
         if (!energySource[boundaryKey]) {
           energySource[boundaryKey] = false;
         }
@@ -84,58 +76,66 @@ function Day17() {
     });
   };
 
-  let energySource: EnergySource = input.reduce(
-    (energySource: EnergySource, row: string, y: number) => {
-      for (let x = 0; x < row.length; x++) {
-        if (row[x] === "#") {
-          energySource[toKey(x, y, 0)] = true;
+  const countActiveCubes = (
+    input: string[],
+    dimensions: number,
+    cycles: number
+  ) => {
+    const neighborOffsets = generateNeighborOffsets(dimensions);
+
+    let energySource: EnergySource = input.reduce(
+      (energySource: EnergySource, row: string, y: number) => {
+        for (let x = 0; x < row.length; x++) {
+          if (row[x] === "#") {
+            const coordinates = new Array(dimensions).fill(0);
+            coordinates[0] = x;
+            coordinates[1] = y;
+            energySource[toKey(coordinates)] = true;
+          }
         }
-      }
-      return energySource;
-    },
-    {}
-  );
+        return energySource;
+      },
+      {}
+    );
+    markInactiveBoundary(energySource, neighborOffsets);
 
-  markInactiveNeighbors(energySource);
+    for (let cycle = 0; cycle < cycles; cycle++) {
+      let newEnergySource: EnergySource = {};
+      Object.entries(energySource).forEach((value: [string, boolean]) => {
+        const activeNeighbors = countActiveNeighbors(
+          toCoordinate(value[0]),
+          energySource,
+          neighborOffsets
+        );
 
-  for (let cycle = 0; cycle < 6; cycle++) {
-    // print each z layer for debugging
-    let newEnergySource: EnergySource = {};
-    Object.entries(energySource).forEach((value: [string, boolean]) => {
-      const activeNeighbors = countActiveNeighbors(
-        toCoordinate(value[0]),
-        energySource
-      );
+        if (
+          (value[1] && (activeNeighbors === 2 || activeNeighbors === 3)) ||
+          (!value[1] && activeNeighbors === 3)
+        ) {
+          newEnergySource[value[0]] = true;
+        }
+      });
 
-      let newState = false;
-      if (value[1]) {
-        // currently active
-        newState = activeNeighbors === 2 || activeNeighbors === 3;
-      } else {
-        // not currently active
-        newState = activeNeighbors === 3;
-      }
+      markInactiveBoundary(newEnergySource, neighborOffsets);
+      energySource = newEnergySource;
+    }
 
-      if (newState) {
-        newEnergySource[value[0]] = true;
-      }
-    });
+    return Object.values(energySource).reduce(
+      (activeCount: number, isActive: boolean) => {
+        return (activeCount += isActive ? 1 : 0);
+      },
+      0
+    );
+  };
 
-    markInactiveNeighbors(newEnergySource);
-    energySource = newEnergySource;
-  }
-
-  const part1 = Object.values(energySource).reduce(
-    (activeCount: number, isActive: boolean) => {
-      return (activeCount += isActive ? 1 : 0);
-    },
-    0
-  );
+  const part1 = countActiveCubes(input, 3, 6);
+  const part2 = countActiveCubes(input, 4, 6);
 
   return (
     <div>
       <h2>Day 17</h2>
       <p>After 6 cycles, there are {part1} active cubes in part 1</p>
+      <p>After 6 cycles, there are {part2} active cubes in part 2</p>
     </div>
   );
 }
